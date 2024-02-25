fe204314b0c350588cd7a05bec165e38f96fe11f
InsaneFirebat <InsaneFirebat@gmail.com>
2024-02-25 16:37:49 -0500

    Fix equipping items from the menu

--- src/macros.asm
+++ src/macros.asm
@@ -170,8 +170,8 @@ macro cm_equipment_item(title, addr, bitmask, inverse)
     db #$28, "        OFF", #$FF
     db #$FF
   .routine
-    LDA.w <addr> : STA !DP_Address
-    LDA.w <addr>>>16 : STA !DP_Address+2
+    LDA.w #<addr> : STA !DP_Address
+    LDA.w #<addr>>>16 : STA !DP_Address+2
     LDA <bitmask> : STA !DP_ToggleValue
     LDA <inverse> : STA !DP_Increment
     JMP equipment_toggle_items
